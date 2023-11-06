0dc68c4ef75ca44fd6e7f5f4c8159aa07247c3bd
Becca Rothwell <5334385+beccarothwell@users.noreply.github.com>
2023-11-06 19:12:24 +0000

    test: Add further tests for different final bonus rolls notation after a strike or spare

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -25,4 +25,24 @@ describe("test different input notation for final bonus roll with strikes and sp
     const scores = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ X";
     expect(calculateBowlingScore(scores)).toBe(155);
   });
+  test(`Strike noted with spaces between bonus strikes - X X X.
+  21 rolls: 9 pairs of 5 and spare, 1 strike, with two bonus strikes = 8 frames * 15 points + 1 frame * 20 points + 1 frame * 30 points = 170`, () => {
+    const scores = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ X X X";
+    expect(calculateBowlingScore(scores)).toBe(170);
+  });
+  test(`Strike noted with no spaces between bonus strikes - XXX.
+  21 rolls: 9 pairs of 5 and spare, 1 strike, with two bonus strikes = 8 frames * 15 points + 1 frame * 20 points + 1 frame * 30 points = 165`, () => {
+    const scores = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ XXX";
+    expect(calculateBowlingScore(scores)).toBe(170);
+  });
+  test(`Strike noted with a space before bonus spare - X 5/.
+  21 rolls: 9 pairs of 5 and spare, 1 strike, with two bonus rolls equaling a spare = 8 frames * 15 points + 2 frames * 20 points = 160`, () => {
+    const scores = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ X 5/";
+    expect(calculateBowlingScore(scores)).toBe(160);
+  });
+  test(`Strike noted with no space before bonus spare - X5/.
+  21 rolls: 9 pairs of 5 and spare, 1 strike, with two bonus rolls equaling a spare = 8 frames * 15 points + 2 frames * 20 points = 160`, () => {
+    const scores = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ X5/";
+    expect(calculateBowlingScore(scores)).toBe(160);
+  });
 });
